688ab3fcc62aa10863f666ff31d70b21446d7c7f
Ernes Nafieiev <demodekoz@gmail.com>
2023-02-10 23:51:30 +0200

    add

--- src/modules/ImagesSearch/ImagesSearchForm/ImagesSearchForm.jsx
+++ src/modules/ImagesSearch/ImagesSearchForm/ImagesSearchForm.jsx
@@ -1,67 +1,109 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './images-search-form.module.scss';
+import initialState from './initialState';
 
-class ImagesSearchForm extends Component {
-  state = {
-    search: '',
-  };
+const ImagesSearchForm = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...initialState });
 
-  handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    setState(prevState => {
+      return { ...prevState, [name]: value };
+    });
   };
 
-  handleSubmit = e => {
-    const { search } = this.state;
+  const handleSubmit = e => {
     e.preventDefault();
     if (search.trim() === '') {
       Notify.info('Please enter what to search for!');
       return;
     }
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.reset();
+    onSubmit({ ...state });
+    setState({ ...initialState });
   };
 
-  reset() {
-    this.setState({
-      search: '',
-    });
-  }
+  const { search } = state;
 
-  render() {
-    const { search } = this.state;
-    const { handleChange, handleSubmit } = this;
-    return (
-      <header className={styles.searchbar}>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <button
-            type="submit"
-            className={styles.button}
-            onSubmit={handleSubmit}
-          >
-            <span className={styles.button__label}>Search</span>
-          </button>
+  return (
+    <header className={styles.searchbar}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <button type="submit" className={styles.button} onSubmit={handleSubmit}>
+          <span className={styles.button__label}>Search</span>
+        </button>
 
-          <input
-            className={styles.input}
-            value={search}
-            onChange={handleChange}
-            name="search"
-            placeholder="Search images and photos"
-            required
-          />
-        </form>
-      </header>
-    );
-  }
-}
-
+        <input
+          className={styles.input}
+          value={search}
+          onChange={handleChange}
+          name="search"
+          placeholder="Search images and photos"
+          required
+        />
+      </form>
+    </header>
+  );
+};
 export default ImagesSearchForm;
 
 ImagesSearchForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
+// class ImagesSearchForm extends Component {
+//   state = {
+//     search: '',
+//   };
+
+//   handleChange = ({ target }) => {
+//     const { name, value } = target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     const { search } = this.state;
+//     e.preventDefault();
+//     if (search.trim() === '') {
+//       Notify.info('Please enter what to search for!');
+//       return;
+//     }
+//     const { onSubmit } = this.props;
+//     onSubmit({ ...this.state });
+//     this.reset();
+//   };
+
+//   reset() {
+//     this.setState({
+//       search: '',
+//     });
+//   }
+
+//   render() {
+//     const { search } = this.state;
+//     const { handleChange, handleSubmit } = this;
+// return (
+//   <header className={styles.searchbar}>
+//     <form className={styles.form} onSubmit={handleSubmit}>
+//       <button
+//         type="submit"
+//         className={styles.button}
+//         onSubmit={handleSubmit}
+//       >
+//         <span className={styles.button__label}>Search</span>
+//       </button>
+
+//       <input
+//         className={styles.input}
+//         value={search}
+//         onChange={handleChange}
+//         name="search"
+//         placeholder="Search images and photos"
+//         required
+//       />
+//     </form>
+//   </header>
+// );
+//   }
+// }
